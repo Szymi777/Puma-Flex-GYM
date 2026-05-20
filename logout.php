ae53051f18da9f9d84f88011f8dae5a5c479290c
Szymi777 <szymi24pl@gmail.com>
2026-05-20 23:39:37 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_unset();    // czyści wszystkie dane sesji
-    session_destroy();  // usuwa plik sesji z serwera
-    header('Location: login.php');
-    exit;
-?>
